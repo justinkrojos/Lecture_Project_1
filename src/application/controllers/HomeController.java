05e337642ae79a6e9ba374a917096fdc4a3c74be
justinkrojos <justinkrojos@gmail.com>
2020-05-03 15:07:18 +1200

    Added mediaplayer after dropping files

--- src/application/controllers/HomeController.java
+++ src/application/controllers/HomeController.java
@@ -1,4 +1,74 @@
 package application.controllers;
 
-public class Controller {
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
+
+public class HomeController {
+
+    @FXML
+            private Pane dragBox;
+
+    private File _draggedFile;
+
+    private Media _video;
+    private MediaPlayer _player;
+    private MediaView _mediaView;
+
+    public void initialize() {
+        dragBox.setOnDragOver(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                if (event.getGestureSource() != dragBox
+                        && event.getDragboard().hasFiles()) {
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+            }
+        });
+
+
+        dragBox.setOnDragDropped(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasFiles()) {
+                    _draggedFile = new File(db.getFiles().toString().substring(1, db.getFiles().toString().length() - 1));
+                    success = true;
+                    //String fileUrl = _draggedFile.getPath();
+                    System.out.println(_draggedFile.getPath());
+                }
+                event.setDropCompleted(success);
+
+                _video = new Media(_draggedFile.toURI().toString());
+                //System.out.println(_draggedFile.toURI().toString());
+                _player = new MediaPlayer(_video);
+
+                _mediaView = new MediaView(_player);
+                _mediaView.fitWidthProperty().bind(dragBox.widthProperty());
+                _mediaView.fitHeightProperty().bind(dragBox.heightProperty());
+                _mediaView.setPreserveRatio(false);
+                dragBox.getChildren().add(_mediaView);
+                _player.play();
+
+                event.consume(); // not dispatched to further listeners
+
+
+
+            }
+        });
+    }
 }
