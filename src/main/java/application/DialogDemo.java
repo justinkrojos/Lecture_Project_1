46389956a89d7c7c2971664bc1561e863ad82f70
justinkrojos <justinkrojos@gmail.com>
2020-05-15 21:05:30 +1200

    Added working sphinx api code

--- src/main/java/application/DialogDemo.java
+++ src/main/java/application/DialogDemo.java
@@ -1,166 +1,48 @@
-package application;/*
- * Copyright 2013 Carnegie Mellon University.
- * Portions Copyright 2004 Sun Microsystems, Inc.
- * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
- * All Rights Reserved.  Use is subject to license terms.
- *
- * See the file "license.terms" for information on usage and
- * redistribution of this file, and for a DISCLAIMER OF ALL
- * WARRANTIES.
- */
-
+package application;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.result.WordResult;
 
 
 public class DialogDemo {
 
-    private static final String ACOUSTIC_MODEL =
-            "resource:/edu/cmu/sphinx/models/en-us/en-us";
-    private static final String DICTIONARY_PATH =
-            "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
+public static void main(String[] args) throws Exception {
 
-    private static final String LANGUAGE_MODEL =
-            "resource:/edu/cmu/sphinx/demo/dialog/weather.lm";
+    System.out.println("Loading models...");
 
-    private static final Map<String, Integer> DIGITS =
-            new HashMap<String, Integer>();
+    Configuration configuration = new Configuration();
 
-    static {
-        DIGITS.put("oh", 0);
-        DIGITS.put("zero", 0);
-        DIGITS.put("one", 1);
-        DIGITS.put("two", 2);
-        DIGITS.put("three", 3);
-        DIGITS.put("four", 4);
-        DIGITS.put("five", 5);
-        DIGITS.put("six", 6);
-        DIGITS.put("seven", 7);
-        DIGITS.put("eight", 8);
-        DIGITS.put("nine", 9);
-    }
+    configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+    configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+    configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-    private static double parseNumber(String[] tokens) {
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i < tokens.length; ++i) {
-            if (tokens[i].equals("point"))
-                sb.append(".");
-            else
-                sb.append(DIGITS.get(tokens[i]));
-        }
+    LiveSpeechRecognizer recognizer =
+            new LiveSpeechRecognizer(configuration);
 
-        return Double.parseDouble(sb.toString());
-    }
-    private static void recognizeDigits(LiveSpeechRecognizer recognizer) {
-        System.out.println("Digits recognition (using GrXML)");
-        System.out.println("--------------------------------");
-        System.out.println("Example: one two three");
-        System.out.println("Say \"101\" to exit");
-        System.out.println("--------------------------------");
 
+    while (true) {
+        System.out.println("a");
         recognizer.startRecognition(true);
-        while (true) {
-            String utterance = recognizer.getResult().getHypothesis();
-            if (utterance.equals("one zero one")
-                    || utterance.equals("one oh one"))
-                break;
-            else
-                System.out.println(utterance);
-        }
-        recognizer.stopRecognition();
-    }
-
-    private static void recognizerBankAccount(LiveSpeechRecognizer recognizer) {
-        System.out.println("This is bank account voice menu");
-        System.out.println("-------------------------------");
-        System.out.println("Example: balance");
-        System.out.println("Example: withdraw zero point five");
-        System.out.println("Example: deposit one two three");
-        System.out.println("Example: back");
-        System.out.println("-------------------------------");
-
-        double savings = .0;
-        recognizer.startRecognition(true);
-
-        while (true) {
-            String utterance = recognizer.getResult().getHypothesis();
-            if (utterance.endsWith("back")) {
-                break;
-            } else if (utterance.startsWith("deposit")) {
-                double deposit = parseNumber(utterance.split("\\s"));
-                savings += deposit;
-                System.out.format("Deposited: $%.2f\n", deposit);
-            } else if (utterance.startsWith("withdraw")) {
-                double withdraw = parseNumber(utterance.split("\\s"));
-                savings -= withdraw;
-                System.out.format("Withdrawn: $%.2f\n", withdraw);
-            } else if (!utterance.endsWith("balance")) {
-                System.out.println("Unrecognized command: " + utterance);
-            }
-
-            System.out.format("Your savings: $%.2f\n", savings);
+        String result = recognizer.getResult().getHypothesis();
+        if (result.startsWith("stop")) {
+            System.out.println("Stopping");
+            recognizer.stopRecognition();
+            return;
+        } else if (result.length() != 0) {
+            System.out.println(result);
+            System.out.println("Printed");
+            recognizer.stopRecognition();
+        } else {
+            System.out.println("Nothing happening");
+            recognizer.stopRecognition();
         }
 
-        recognizer.stopRecognition();
     }
-
-    private static void recognizeWeather(LiveSpeechRecognizer recognizer) {
-        System.out.println("Try some forecast. End with \"the end\"");
-        System.out.println("-------------------------------------");
-        System.out.println("Example: mostly dry some fog patches tonight");
-        System.out.println("Example: sunny spells on wednesday");
-        System.out.println("-------------------------------------");
-
-        recognizer.startRecognition(true);
-        while (true) {
-            String utterance = recognizer.getResult().getHypothesis();
-            if (utterance.equals("the end"))
-                break;
-            else
-                System.out.println(utterance);
-        }
-        recognizer.stopRecognition();
-    }
-
-    public static void main(String[] args) throws Exception {
-        Configuration configuration = new Configuration();
-        configuration.setAcousticModelPath(ACOUSTIC_MODEL);
-        configuration.setDictionaryPath(DICTIONARY_PATH);
-        configuration.setUseGrammar(true);
-
-        configuration.setGrammarName("dialog");
-        LiveSpeechRecognizer jsgfRecognizer =
-                new LiveSpeechRecognizer(configuration);
-
-        jsgfRecognizer.startRecognition(true);
-        while (true) {
-            System.out.println("Choose menu item:");
-            System.out.println("Example: go to the bank account");
-            System.out.println("Example: exit the program");
-            System.out.println("Example: weather forecast");
-            System.out.println("Example: digits\n");
-
-            String utterance = jsgfRecognizer.getResult().getHypothesis();
-
-            if (utterance.startsWith("exit"))
-                break;
-
-
-
-            if (utterance.equals("bank account")) {
-                jsgfRecognizer.stopRecognition();
-                recognizerBankAccount(jsgfRecognizer);
-                jsgfRecognizer.startRecognition(true);
-            }
-
-
-        }
-
-        jsgfRecognizer.stopRecognition();
-    }
+}
 }
--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -20,15 +20,6 @@ public class Main extends Application {
     private static Stage stage;
     private static Stage notFs;
     private static Scene scene;
-   // private static final String ACOUSTIC_MODEL =
-     //       "resource:/edu/cmu/sphinx/models/en-us/en-us";
-    private static final String DICTIONARY_PATH =
-            "resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict";
-    private static final String GRAMMAR_PATH =
-            "resource:/edu/cmu/sphinx/demo/dialog/";
-    private static final String LANGUAGE_MODEL =
-            "resource:/edu/cmu/sphinx/demo/dialog/weather.lm";
-    private static Configuration config;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -40,6 +31,38 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+
+        System.out.println("Loading models...");
+
+        Configuration configuration = new Configuration();
+
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+
+        LiveSpeechRecognizer recognizer =
+                new LiveSpeechRecognizer(configuration);
+
+
+        while (true) {
+            System.out.println("a");
+            recognizer.startRecognition(true);
+            String result = recognizer.getResult().getHypothesis();
+            if (result.startsWith("stop")) {
+                System.out.println("Stopping");
+                recognizer.stopRecognition();
+                return;
+            } else if (result.length() != 0) {
+                System.out.println(result);
+                System.out.println("Printed");
+                recognizer.stopRecognition();
+            } else {
+                System.out.println("Nothing happening");
+                recognizer.stopRecognition();
+            }
+
+        }
 
         }
 
